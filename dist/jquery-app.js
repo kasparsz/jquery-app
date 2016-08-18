459dced85b22eda803e59ddc33d1cf73042531d8
Kaspars Zuks <kaspars.zuks@videinfra.com>
2016-08-18 15:45:49 +0300

    Added tests

--- dist/jquery-app.js
+++ dist/jquery-app.js
@@ -5,117 +5,119 @@
  * Licensed under the MIT License.
  */
 
-var $ = jQuery;
-var REGEX_SPLIT = /\s*,\s*/;
-var REGEX_NOT_LOWERCASE = /[^a-z]/;
-var PROPERTY_NAME = 'jQueryAppData';
+(function (global) {
+    var $ = global.jQuery || window.jQuery;
+    var REGEX_SPLIT = /\s*,\s*/;
+    var REGEX_NOT_LOWERCASE = /[^a-z]/;
+    var PROPERTY_NAME = 'jQueryAppData';
 
-$.app = {
-    settings: {
-        'namespace': 'plugin'
-    },
+    $.app = {
+        settings: {
+            'namespace': 'plugin'
+        },
 
-    /**
-     * Call all plugins for element
-     *
-     * @param {object} $element jQuery element
-     */
-    call: function ($element) {
-        var this$1 = this;
+        /**
+         * Call all plugins for element
+         *
+         * @param {object} $element jQuery element
+         */
+        call: function (element) {
+            var $element = $(element);
+            var plugins  = $.app.getPlugins($element);
+            var   data     = $element.data(PROPERTY_NAME);
 
-        var plugins  = getPlugins($element);
-        var   data     = $element.data(PROPERTY_NAME);
+            if (!data) {
+                $element.data(PROPERTY_NAME, data = {});
+            }
 
-        if (!data) {
-            $element.data(PROPERTY_NAME, data = {});
+            plugins.forEach(function (plugin) {
+                // Eech plugin called only once on each element
+                if (!data[plugin]) {
+                    data[plugin] = true;
+
+                    // As only argument pass plugin options
+                    var options = $.app.getPluginOptions($element, plugin);
+                    $element[plugin](options);
+                }
+            });
+        },
+
+        /**
+         * Returns list of plugin names for element
+         *
+         * @param {object} $element jQuery element
+         * @returns {array} List of plugin names
+         */
+        getPlugins: function ($element) {
+            var plugins = $element.data($.app.settings.namespace).split(REGEX_SPLIT);
+
+            return plugins.filter(function (plugin) {
+                return (typeof $.fn[plugin] === 'function');
+            });
+        },
+
+        /**
+         * Returns list of options/settings for specific plugin for element
+         *
+         * @param {object} $element jQuery element
+         * @param {string} plugin Plugin name
+         * @returns {object} Plugin options/settings
+         */
+        getPluginOptions: function ($element, plugin) {
+            // data-PLUGIN="VALUE",  data-PLUGIN-PROPERTY="VALUE"
+            var attrs   = $element.get(0).attributes;
+            var prefix  = "data-" + plugin;
+            var options = {};
+
+            var data    = $element.data();
+
+            for (var key in data) {
+                var value = data[key];
+
+                if (key === plugin) {
+                    $.extend(options, value);
+                }  else if (key.indexOf(plugin) === 0 && key.substr(plugin.length, 1).match(REGEX_NOT_LOWERCASE)) {
+                    var name = key.substr(plugin.length);
+                    name = name[0].toLowerCase() + name.substr(1);
+
+                    options[name] = value;
+                }
+            }
+
+            /*
+            for (let i = 0, ii = attrs.length; i < ii; i++) {
+                let name = attrs[i].name;
+
+                if (name === prefix) {
+                    // data-PLUGIN="JSON"
+                    $.extend(options, $element.data(plugin));
+                } else if (name.indexOf(prefix + '-') === 0) {
+                    // data-PLUGIN-PROPERTY="VALUE"
+                    let prop = name.substr(prefix.length + 1); // attribute name without "data-plugin-"
+                    options[$.camelCase(prop)] = value;
+                }
+            }
+            */
+
+            return options;
         }
 
-        plugins.forEach(function (plugin) {
-            // Eech plugin called only once on each element
-            if (!data[plugin]) {
-                data[plugin] = true;
+    };
 
-                // As only argument pass plugin options
-                var options = this$1.getPluginOptions($element, plugin);
-                $element[plugin](options);
-            }
-        });
-    },
 
     /**
-     * Returns list of plugin names for element
+     * jQuery plugin to initialize all plugins
      *
-     * @param {object} $element jQuery element
-     * @returns {array} List of plugin names
+     * @param {object?} options jQuery App settings
      */
-    getPlugins: function ($element) {
-        var plugins = $element.data($.app.settings.namespace).split(REGEX_SPLIT);
+    $.fn.app = function (options) {
+        var settings  = $.extend($.app.settings, options);
+        var selector  = "[data-" + (settings.namespace) + "]";
+        var $elements = this.find(selector).addBack(selector);
 
-        return plugins.filter(function (plugin) {
-            return (typeof $.fn[plugin] === 'function');
-        });
-    },
+        $elements.each(function (index, element) { return $.app.call(element); });
 
-    /**
-     * Returns list of options/settings for specific plugin for element
-     *
-     * @param {object} $element jQuery element
-     * @param {string} plugin Plugin name
-     * @returns {object} Plugin options/settings
-     */
-    getPluginOptions: function ($element, plugin) {
-        // data-PLUGIN="VALUE",  data-PLUGIN-PROPERTY="VALUE"
-        var attrs   = $element.get(0).attributes;
-        var prefix  = "data-" + plugin;
-        var options = {};
+        return this;
+    };
 
-        var data    = $element.data();
-
-        for (var key in data) {
-            var value = data[key];
-
-            if (key === plugin) {
-                $.extend(options, value);
-            }  else if (key.indexOf(plugin) === 0 && key.substr(plugin.length, 1).match(REGEX_NOT_LOWERCASE)) {
-                var name = key.substr(plugin.length);
-                name = name[0].toLowerCase() + name.substr(1);
-
-                options[name] = value;
-            }
-        }
-
-        /*
-        for (let i = 0, ii = attrs.length; i < ii; i++) {
-            let name = attrs[i].name;
-
-            if (name === prefix) {
-                // data-PLUGIN="JSON"
-                $.extend(options, $element.data(plugin));
-            } else if (name.indexOf(prefix + '-') === 0) {
-                // data-PLUGIN-PROPERTY="VALUE"
-                let prop = name.substr(prefix.length + 1); // attribute name without "data-plugin-"
-                options[$.camelCase(prop)] = value;
-            }
-        }
-        */
-
-        return options;
-    }
-
-};
-
-
-/**
- * jQuery plugin to initialize all plugins
- *
- * @param {object?} options jQuery App settings
- */
-$.fn.app = function (options) {
-    var settings  = $.extend($.app.settings, options);
-    var selector  = "[data-" + (settings.namespace) + "]";
-    var $elements = this.find(selector).addBack(selector);
-
-    $elements.each(function (index, element) { return $.app.call(element); });
-
-    return this;
-};
+})(this);
--- test.js
+++ test.js
@@ -5,15 +5,17 @@ var assert = require('assert');
 
 var fs = require('fs');
 var jsdom = require('jsdom');
+
 var jQuery = fs.readFileSync('./node_modules/jquery/dist/jquery.js', "utf-8");
-var generate = require('./dist/jquery-app.js');
+var jQueryApp = fs.readFileSync('./dist/jquery-app.js', "utf-8");
 
 function createTestEnv(fn) {
     return function (done) {
-        jsdom.env('', {
-            src: [jQuery],
+        jsdom.env('<html><body></body></html>', {
+            src: [jQuery, jQueryApp],
             done: function (err, window) {
                 if (err) throw err; // everything should fail
+
                 fn(done, window.$);
                 window.close(); // clean up memory
             }
@@ -23,6 +25,67 @@ function createTestEnv(fn) {
 
 describe('jquery-app', function(done) {
 
+    it('$.fn.app plugin exists', createTestEnv(function (done, $) {
+        assert.equal(typeof $.fn.app, 'function');
+        done();
+    }));
 
+    it('should call plugin', createTestEnv(function (done, $) {
+        var foo = 0;
+        var bar = 0;
+
+        $.fn.foo = function () { foo++; };
+        $.fn.bar = function () { bar++; };
+
+        $('body').append('<div data-plugin="foo"><span data-plugin="bar"></span></div>').app();
+
+        assert.equal(foo, 1);
+        assert.equal(bar, 1);
+        done();
+    }));
+
+    it('should call plugin only once', createTestEnv(function (done, $) {
+        var foo = 0;
+
+        $.fn.foo = function () { foo++; };
+
+        $('body').append('<div data-plugin="foo"><span data-plugin="bar"></span></div>').app().app();
+
+        assert.equal(foo, 1);
+        done();
+    }));
+
+    it('should have options object', createTestEnv(function (done, $) {
+
+        $.fn.test = function (options) {
+            assert.equal(typeof options, 'object');
+            assert.equal(Object.keys(options).length, 0);
+            done();
+        };
+
+        $('body').append('<div data-plugin="test"></div>').app();
+    }));
+
+    it('should pass options from data-PLUGIN attribute', createTestEnv(function (done, $) {
+
+        $.fn.test = function (options) {
+            assert.equal(options.name, 'John');
+            assert.equal(options.age, 21);
+            done();
+        };
+
+        $('body').append('<div data-plugin="test" data-test=\'{"name": "John", "age": 21}\'></div>').app();
+    }));
+
+    it('should pass options from data-PLUGIN-PROPERTY attributes', createTestEnv(function (done, $) {
+
+        $.fn.test = function (options) {
+            assert.equal(options.name, 'John');
+            assert.equal(options.age, 21);
+            done();
+        };
+
+        $('body').append('<div data-plugin="test" data-test-name="John" data-test-age="21"></div>').app();
+    }));
 
 });
